341ba7043ee59aa48042277a1223fa497bd1aa4b
Ákos Zagyva <fortunevideok@gmail.com>
2021-03-02 00:31:00 +0100

    added .mvn/jvm.config removed lombok support from the pom.xml added record GeoLocation

---
+++ src/main/java/GeoLocation.java
@@ -0,0 +1,56 @@
+package geolocator;
+public record GeoLocation
+//{
+
+(
+String as,
+String city,
+String country,
+String countryCode,
+String isp,
+double lat,
+double lon,
+String org,
+String query,
+String region,
+String regionName,
+String timezone,
+String zip
+)   {}
+
+
+/*
+System.out.println(GeoLocation.as());
+System.out.println(GeoLocation.city());
+System.out.println(GeoLocation.country());
+System.out.println(GeoLocation.countryCode());
+System.out.println(GeoLocation.isp());
+System.out.println(GeoLocation.lat());
+System.out.println(GeoLocation.lon());
+System.out.println(GeoLocation.org());
+System.out.println(GeoLocation.query());
+System.out.println(GeoLocation.region());
+System.out.println(GeoLocation.regionName());
+System.out.println(GeoLocation.timezone());
+System.out.println(GeoLocation.zip());
+
+
+
+
+*/
+/*
+    private String as;
+    private String city;
+    private String country;
+    private String countryCode;
+    private String isp;
+    private double lat;
+    private double lon;
+    private String org;
+    private String query;
+    private String region;
+    private String regionName;
+    private String timezone;
+    private String zip;
+*/
+//}
